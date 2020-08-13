7b4ac953b0c9179dad3faa38a0c840d89a28d95b
Farzad FARID <farzad.farid@kapten.com>
2020-08-13 22:39:50 +0200

    Implement Set 1 / Challenge 2

---
+++ src/set1/challenge2.rs
@@ -0,0 +1,20 @@
+use cryptopals::{helper, crypto};
+
+// Set 1 / Challenge 2
+pub fn main() {
+    helper::section("Set 1 / Challenge 2");
+
+    let input1 = "1c0111001f010100061a024b53535009181c";
+    let input2 = "686974207468652062756c6c277320657965";
+    let expected_output = "746865206b696420646f6e277420706c6179";
+
+    let output = crypto::bytes2hex(
+        &crypto::xor_arrays(
+            &crypto::hex2bytes(input1).unwrap(),
+            &crypto::hex2bytes(input2).unwrap()
+        )
+    );
+
+    println!("{} ^ {} = {}", input1, input2, output);
+    assert_eq!(expected_output, output);
+}
--- src/set1/mod.rs
+++ src/set1/mod.rs
@@ -1,7 +1,9 @@
 // Set 1
 
 mod challenge1;
+mod challenge2;
 
 pub fn main() {
     challenge1::main();
+    challenge2::main();
 }
